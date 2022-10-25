e782a818d52008b7eb801a68d664637d6747cfa0
javdev <javier.viera007@gmail.com>
2022-10-24 21:42:09 -0300

    add json events DOM

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "specie": "Human",
+        "description": "Rick Sanchez description"
+    },
+    {
+        "name": "Alien Morty",
+        "status": "missing",
+        "specie": "Alien",
+        "description": "Alien Morty description"
+    },
+    {
+        "name": "Adjudicator Rick",
+        "status": "dead",
+        "specie": "Human",
+        "description": "Adjudicator Rick description"
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,31 +1,67 @@
+function init() {
+    let firstCard = document.getElementById('main-card');
+    let secondCard = document.getElementById('main-card-2');
+    let thirdCard = document.getElementById('main-card-3');
 
+    firstCard.addEventListener("click", onCardClick, false);
+    secondCard.addEventListener("click", onCardClick, false);
+    thirdCard.addEventListener("click", onCardClick, false);
 
-function agarrarNumero(){
-    const numero = Number(prompt("ingresa un numero "));
-    if(numero===0){
-        return 10;
-    }
-    return numero;
-    
-
-}
- 
-console.log(agarrarNumero);
-const numero = agarrarNumero()
-
-for(let i=1; i<=numero; i++){
-    
-
-    if(i%2===0 && i!=0){
-        document.write(`<p>${i} es un numero par</p> `)
-
-    }
-    else{
-        document.write(`<p>${i} es un numero impar</p> `)
-
-    }
-
+    // Data imported from json
+    localStorage.setItem("characters", JSON.stringify(data));
+    fillCardSpecies();
+    fillCardNames();
+    fillCardStatus();
 }
 
- 
+document.addEventListener("DOMContentLoaded", init, false);
 
+function fillCardSpecies() {
+    const characters = JSON.parse(localStorage.getItem("characters"));
+
+    characters.forEach((character, index) => {
+        const textElement = document.getElementById(`specie-${index + 1}`);
+        textElement.append(character.specie);
+    });
+}
+
+function fillCardNames() {
+    const characters = JSON.parse(localStorage.getItem("characters"));
+
+    characters.forEach((character, index) => {
+        const textElement = document.getElementById(`name-${index + 1}`);
+        textElement.append(character.name);
+    });
+}
+
+function fillCardStatus() {
+    const characters = JSON.parse(localStorage.getItem("characters"));
+
+    characters.forEach((character, index) => {
+        const textElement = document.getElementById(`status-${index + 1}`);
+        textElement.append(character.status);
+    });
+}
+
+function onCardClick(event) {
+    let cardId = event.currentTarget.id;
+    let descriptionElement = document.getElementById('card-description');
+    descriptionElement.textContent = '';
+    const characters = JSON.parse(localStorage.getItem("characters"));
+
+    const firstCardText = characters[0].description;
+    const secondCardText = characters[1].description;
+    const thirdCardText = characters[2].description;
+
+    switch (cardId) {
+        case "main-card":
+            descriptionElement.append(firstCardText);
+            break;
+        case "main-card-2":
+            descriptionElement.append(secondCardText);
+            break;
+        case "main-card-3":
+            descriptionElement.append(thirdCardText);
+            break;
+    }
+}
