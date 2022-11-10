faaa65b41272105aad898f4cedf56ef3ce1b9a7a
javdev <javier.viera007@gmail.com>
2022-11-09 22:52:50 -0300

    proyecto final

--- js/main.js
+++ js/main.js
@@ -1,67 +1,94 @@
-function init() {
-    let firstCard = document.getElementById('main-card');
-    let secondCard = document.getElementById('main-card-2');
-    let thirdCard = document.getElementById('main-card-3');
+async function init() {
+    // Data imported from Rick and Morty api
+    let allCharacters = await fetchAllCharacters();
+
+    let firstCard = document.getElementById('main-card-0');
+    let secondCard = document.getElementById('main-card-1');
+    let thirdCard = document.getElementById('main-card-2');
+    let fourthCard = document.getElementById('main-card-3');
 
     firstCard.addEventListener("click", onCardClick, false);
     secondCard.addEventListener("click", onCardClick, false);
     thirdCard.addEventListener("click", onCardClick, false);
+    fourthCard.addEventListener("click", onCardClick, false);
 
-    // Data imported from json
-    localStorage.setItem("characters", JSON.stringify(data));
-    fillCardSpecies();
-    fillCardNames();
-    fillCardStatus();
+    // Modify to grab different sets (4 only, up to 20)
+    const SLICE_START = 16;
+    const SLICE_END = 20;
+
+    // Lodash to use only first 4 characters
+    const characters = _.slice(allCharacters, [start=SLICE_START], [end=SLICE_END]);
+
+    fillCardSpecies(characters);
+    fillCardNames(characters);
+    fillCardStatus(characters);
+    fillCardImages(characters);
+}
+
+async function fetchAllCharacters() {
+    let data = await fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+    return data.results;
+}
+
+async function fetchCharacterData(characterIndex) {
+    let data = await fetch(`https://rickandmortyapi.com/api/character/${characterIndex}`)
+        .then(response => response.json());
+    return data;
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
 
-function fillCardSpecies() {
-    const characters = JSON.parse(localStorage.getItem("characters"));
-
+function fillCardSpecies(characters) {
     characters.forEach((character, index) => {
-        const textElement = document.getElementById(`specie-${index + 1}`);
-        textElement.append(character.specie);
+        const textElement = document.getElementById(`specie-${index}`);
+        textElement.append(character.species);
     });
 }
 
-function fillCardNames() {
-    const characters = JSON.parse(localStorage.getItem("characters"));
-
+function fillCardNames(characters) {
     characters.forEach((character, index) => {
-        const textElement = document.getElementById(`name-${index + 1}`);
+        const textElement = document.getElementById(`name-${index}`);
         textElement.append(character.name);
     });
 }
 
-function fillCardStatus() {
-    const characters = JSON.parse(localStorage.getItem("characters"));
-
+function fillCardStatus(characters) {
     characters.forEach((character, index) => {
-        const textElement = document.getElementById(`status-${index + 1}`);
-        textElement.append(character.status);
+        const status = character.status;
+        const textElement = document.getElementById(`status-${index}`);
+        switch(status) {
+            case "Alive":
+                textElement.style.background = '#36b37e';
+                break;
+            case "Dead":
+                textElement.style.background = '#ff5630';
+                break;
+            case "Missing":
+                textElement.style.background = '#3a3a3a';
+                break;
+            default:
+                textElement.style.background = '#f1f509';
+                break;
+        }
+        textElement.append(status);
     });
 }
 
-function onCardClick(event) {
-    let cardId = event.currentTarget.id;
-    let descriptionElement = document.getElementById('card-description');
-    descriptionElement.textContent = '';
-    const characters = JSON.parse(localStorage.getItem("characters"));
+function fillCardImages(characters) {
+    characters.forEach((character, index) => {
+        const imageElement = document.getElementById(`card-img-${index}`);
+        const imageUrl = character.image;
+        imageElement.style.backgroundImage = `url(${imageUrl})`;
+    });
+}
 
-    const firstCardText = characters[0].description;
-    const secondCardText = characters[1].description;
-    const thirdCardText = characters[2].description;
-
-    switch (cardId) {
-        case "main-card":
-            descriptionElement.append(firstCardText);
-            break;
-        case "main-card-2":
-            descriptionElement.append(secondCardText);
-            break;
-        case "main-card-3":
-            descriptionElement.append(thirdCardText);
-            break;
-    }
+async function onCardClick(event) {
+    // Get character index from id
+    const characterIndex = parseInt(event.currentTarget.id.split('-').pop() + 1);
+    const character = await fetchCharacterData(characterIndex);
+    const characterLocation = character.location.name;
+    let locationElement = document.getElementById('card-location');
+    locationElement.textContent = '';
+    locationElement.append(characterLocation);
 }
